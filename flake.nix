865e46c6a187cc9fcd45ad45eb2fc3e8a4b732c2
mike <h@solfunmeme.com>
2025-10-05 13:58:36 +0000

    Feat: Add gemini-cli input and revert base-job URL in flake.nix
    - Added gemini-cli as an input and argument to the flake.nix.
    - Reverted base-job.url to use feature/foaf branch and original dir attribute.
    - Commented out the apps section for debugging purposes.

--- flake.nix
+++ flake.nix
@@ -2,14 +2,15 @@
   description = "Nix Task Runner";
 
   inputs = {
-  inputs.nixpkgs.url = "github:meta-introspector/nixpkgs?ref=feature/CRQ-016-nixify";
+    nixpkgs.url = "github:meta-introspector/nixpkgs?ref=feature/CRQ-016-nixify";
     utils.url = "github:meta-introspector/flake-utils";
     yarnpnp2nix = { url = "github:meta-introspector/yarnpnp2nix"; inputs.nixpkgs.follows = "nixpkgs"; inputs.utils.follows = "utils"; };
+    gemini-cli.url = "github:meta-introspector/gemini-cli?ref=feature/working-gemini-cli-nix-store";
 
     base-job.url = "github:meta-introspector/time-2025/feature/foaf?dir=09/26/jobs/vendor/nix-task/nix/base-job";
   };
 
-  outputs = inputs@{ self, nixpkgs, utils, base-job, ... }:
+  outputs = inputs@{ self, nixpkgs, utils, base-job, gemini-cli, ... }:
     let
       nixpkgsLib = nixpkgs.lib;
     in
@@ -66,15 +67,16 @@
           solana-nix-trigger-interpreter = pkgs.callPackage ./nix/tasks/solana-nix-trigger-interpreter.nix { inherit pkgs; };
         };
 
-        apps = {
-          gemini = base-job.lib.mkBaseJob { name = "gemini"; runScript = tasks.gemini.run; };
-          run-gemini-cli = base-job.lib.mkBaseJob { name = "run-gemini-cli"; runScript = tasks.run-gemini-cli.run; };
-          solana-ai-trigger = base-job.lib.mkBaseJob { name = "solana-ai-trigger"; runScript = tasks.solana-ai-trigger.run; };
-          process-solana-nar = base-job.lib.mkBaseJob { name = "process-solana-nar"; runScript = tasks.process-solana-nar.run; };
-          helius-block-processor = { type = "app"; program = "${tasks.helius-block-processor.run}"; };
-          solana-nix-trigger-interpreter = { type = "app"; program = "${tasks.solana-nix-trigger-interpreter.run}"; };
-        };
+        # apps = {
+        #   gemini = base-job.lib.mkBaseJob { name = "gemini"; runScript = tasks.gemini.run; };
+        #   run-gemini-cli = base-job.lib.mkBaseJob { name = "run-gemini-cli"; runScript = tasks.run-gemini-cli.run; };
+        #   solana-ai-trigger = base-job.lib.mkBaseJob { name = "solana-ai-trigger"; runScript = tasks.solana-ai-trigger.run; };
+        #   process-solana-nar = base-job.lib.mkBaseJob { name = "process-solana-nar"; runScript = tasks.tasks.process-solana-nar.run; };
+        #   helius-block-processor = { type = "app"; program = "${tasks.helius-block-processor.run}"; };
+        #   solana-nix-trigger-interpreter = { type = "app"; program = "${tasks.solana-nix-trigger-interpreter.run}"; };
+        # };
       }
     )) // {
       lib = import ./nix/lib { lib = nixpkgsLib; };
     };
+}
